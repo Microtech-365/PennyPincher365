47ddbae93dac98764eecbe11412aacab18880e8e
Sheraz Khan <sherazkhanhooah@gmail.com>
2025-09-10 19:24:28 +0000

    Make background match the rest of the page background (_for element div_

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -52,7 +52,7 @@ export default function SignupPage() {
 
   return (
     <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
-       <div className="hidden bg-black lg:flex items-center justify-center p-8">
+       <div className="hidden bg-background lg:flex items-center justify-center p-8">
         {dashboardImage && (
             <Image
                 src={dashboardImage.imageUrl}
